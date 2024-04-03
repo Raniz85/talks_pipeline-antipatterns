8e82f6a2854f0b20cf201a776b50e3ea801e1def
Daniel Raniz Raneland <raniz@factor10.com>
2024-04-03 16:34:20 +0200

    Add custom component to mark slide as an antipattern

---
+++ components/AntiPattern.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+
+const props = defineProps({
+  num: {
+  },
+})
+</script>
+
+<template>
+  <div class="absolute top-5px left-5px text-left">
+    <h2>Anti-pattern #{{ props.num }}</h2>
+  </div>
+</template>
